2ffd3a67324794b8939dea7dd7c2a1415ad28b7e
tranthungan <you@example.com>
2019-06-02 20:38:51 +0700

    edit modal

--- backend/roles/js/2.js
+++ backend/roles/js/2.js
@@ -33,62 +33,49 @@ else if(updated_at === ""){
 return isEmpty;
 }
 
-// add Row
-function addHtmlTableRow()
+
+// display selected row data into input text
+function selectedRowToInput()
 {
- // get the table by id
- // create a new row and cells
- // get value from input text
- // set the values into row cell's
- if(!checkEmptyInput()){
- 	var newRow = table.insertRow(table.length),
-  cell1 = newRow.insertCell(0),
- 	cell2 = newRow.insertCell(1),
-  cell3 = newRow.insertCell(2),
-  cell4 = newRow.insertCell(3),
-  cell5 = newRow.insertCell(4),
-  number = document.getElementById("number").value,
- 	role = document.getElementById("role").value;
-  created_at = document.getElementById("created_at").value,
-  updated_at = document.getElementById("updated_at").value;
-  cell1.innerHTML = number; 
- 	cell2.innerHTML = role;
-  cell3.innerHTML = created_at;
-  cell4.innerHTML = updated_at;
-  cell5.innerHTML = "<a class='btn btn-primary'><i class='fas fa-pencil-alt' style='color:white'></i></a> <a class='btn btn-danger'><i class='fas fa-pencil-alt' style='color:white'></i></a>";
- // call the function to set the event to the new row
- // selectedRowToInput();
+
+for(var i = 1; i < table.rows.length; i++)
+{
+table.rows[i].onclick = function()
+{
+       // get the seected row index
+       rIndex = this.rowIndex;
+       console.log(rIndex);
+       document.getElementById("number").value = this.cells[0].innerHTML;
+       document.getElementById("role").value = this.cells[1].innerHTML;
+       document.getElementById("created_at").value = this.cells[2].innerHTML;
+       document.getElementById("updated_at").value = this.cells[3].innerHTML;
+      };
+     }
+    }
+    selectedRowToInput();
+
+    function editHtmlTbleSelectedRow()
+    {
+      var number = document.getElementById("number").value,
+          role = document.getElementById("role").value,
+          created_at = document.getElementById("created_at").value,
+          updated_at = document.getElementById("updated_at").value;   
+    	if(!checkEmptyInput()){
+    		table.rows[rIndex].cells[0].innerHTML = number;
+        table.rows[rIndex].cells[1].innerHTML = role;
+        table.rows[rIndex].cells[2].innerHTML = created_at;
+        table.rows[rIndex].cells[3].innerHTML = updated_at;
+    	}
+    }
+
+    function removeSelectedRow()
+    {
+ {
+     table.deleteRow(rIndex);
+     // clear input text
+     document.getElementById("number").value = "";
+     document.getElementById("role").value = "";
+     document.getElementById("created_at").value = "";
+     document.getElementById("updated_at").value = "";
+ }
 }
-}
-
-// // display selected row data into input text
-// function selectedRowToInput()
-// {
-
-// for(var i = 1; i < table.rows.length; i++)
-// {
-// table.rows[i].onclick = function()
-// {
-//        // get the seected row index
-//        rIndex = this.rowIndex;
-//        document.getElementById("role").value = this.cells[0].innerHTML;
-
-//       };
-//      }
-//     }
-//     selectedRowToInput();
-
-//     function editHtmlTbleSelectedRow()
-//     {
-//     	var role = document.getElementById("role").value;
-//     	if(!checkEmptyInput()){
-//     		table.rows[rIndex].cells[0].innerHTML = role;
-//     	}
-//     }
-
-//     function removeSelectedRow()
-//     {
-//     	table.deleteRow(rIndex);
-//  // clear input text
-//  document.getElementById("role").value = "";
-// }
